62dd4691b10aab029e3289f9571387389642201f
lucasSPro <lucas.san.77@gmail.com>
2020-04-25 23:10:37 -0300

    adds user registration relating to appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import appointmentRouter from './appointments.route';
+import usersRouter from './users.route';
+import sessionsRouter from './sessions.route';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
