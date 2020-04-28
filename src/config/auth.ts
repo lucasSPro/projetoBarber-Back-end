4bed291051995186ce7d765f521fe431e8fb2bde
lucasSPro <lucas.san.77@gmail.com>
2020-04-27 22:44:09 -0300

    adds user authentication & file upload

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '3edd20a96fc45f890d85d5d7520ca354',
+    expiresIn: '1d',
+  },
+};
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
-const UsersRouter = Router();
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
-UsersRouter.post('/', async (request, response) => {
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
     const createUser = new CreateUserService();
@@ -20,4 +26,26 @@ UsersRouter.post('/', async (request, response) => {
   }
 });
 
-export default UsersRouter;
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json({ user });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
+
+export default usersRouter;
